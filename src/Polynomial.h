fc793839822188f366874fe85918496194d75422
piotrkali <261696@student.pwr.edu.pl>
2023-01-31 16:32:43 +0100

    polynomial

--- src/Polynomial.h
+++ src/Polynomial.h
@@ -6,25 +6,57 @@
 #define GF_POLYNOMIAL_H
 #include "vector"
 #include "math.h"
+#include <algorithm>
+#include <iostream>
 template <class T>
 class Polynomial {
 
-
+    void populate_with_zeros(int p, int q);
 public:
     std::vector<T> coefficients;    //indeks wektora to potega zmiennej,\
     0 na k-tej pozycji oznacza 0=0*x^k
-    Polynomial();
-    explicit Polynomial(std::vector<T> vector);
+    Polynomial(T zero_flag);
+    Polynomial(int size, T zero_flag);
+    explicit Polynomial(std::vector<T> vector, T zero_flag);
+    Polynomial(const Polynomial<T>& polynomial);
     Polynomial<T> operator +(const Polynomial<T>& p);
     Polynomial<T> operator -(const Polynomial<T>& p);
     Polynomial<T> operator *(const Polynomial<T>& p);
-
+    Polynomial<T> operator /(const Polynomial<T>& p);
+    Polynomial<T> operator %(const Polynomial<T>& p);
+    bool operator ==(const Polynomial<T>& p);
+    bool operator !=(const Polynomial<T>& p);
     long evaluate(double x);
-
+    T zero_flag;
+    void clean();
     //friend std::istream& operator >>(std::istream& is, const Polynomial<T>& p);
 
 };
-
+template<class T>
+bool Polynomial<T>::operator ==(const Polynomial<T>& p) {
+    if (this->coefficients.size() != p.coefficients.size()) {
+        return false;
+    }
+    for (int i = 0; i < this->coefficients.size(); i++) {
+        if (this->coefficients[i] != p.coefficients[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+template<class T>
+bool Polynomial<T>::operator !=(const Polynomial<T>& p) {
+    if (this->coefficients.size() != p.coefficients.size()) {
+        return true;
+    }
+    
+    for (int i = 0; i < this->coefficients.size(); i++) {
+        if (this->coefficients[i] != p.coefficients[i]) {
+            return true;
+        }
+    }
+    return false;
+}
 template<class T>
 Polynomial<T> Polynomial<T>::operator+(const Polynomial<T> &p) {
     std::vector<T> v;
@@ -43,14 +75,14 @@ Polynomial<T> Polynomial<T>::operator+(const Polynomial<T> &p) {
             v.push_back(p.coefficients[i]);
         }
     }
-    return Polynomial<T>(v);
+    return Polynomial<T>(v, zero_flag);
 }
 
 template<class T>
-Polynomial<T>::Polynomial() = default;
+Polynomial<T>::Polynomial(T zero_flag) : zero_flag(zero_flag){}
 
 template<class T>
-Polynomial<T>::Polynomial(std::vector<T> vector) : coefficients(vector) {
+Polynomial<T>::Polynomial(std::vector<T> vector, T zero_flag) : coefficients(vector), zero_flag(zero_flag) {
 
 }
 
@@ -72,7 +104,7 @@ Polynomial<T> Polynomial<T>::operator-(const Polynomial<T> &p) {
             v.push_back(0-p.coefficients[i]);
         }
     }
-    return Polynomial<T>(v);
+    return Polynomial<T>(v, zero_flag);
 }
 
 template<class T>
@@ -83,7 +115,7 @@ Polynomial<T> Polynomial<T>::operator*(const Polynomial<T> &p) {
             vector[i + j] += p.coefficients[j] * this->coefficients[i];
         }
     }
-    return Polynomial<T>(vector);
+    return Polynomial<T>(vector, zero_flag);
 }
 
 template<class T>
@@ -96,13 +128,113 @@ long Polynomial<T>::evaluate(double x) {
 }
 
 template<class T>
+Polynomial<T> Polynomial<T>::operator/(const Polynomial<T> &q) {
+    std::vector<T> dividend = this->coefficients;
+    std::reverse(dividend.begin(), dividend.end());
+    std::vector<T> divisor = q.coefficients;
+    std::reverse(divisor.begin(), divisor.end());
+    std::vector<T> quotient;
+	quotient = dividend;
+    
+	int normalizer = divisor[0];
+	
+	for (int i = 0; i < dividend.size() - (divisor.size() - 1); i++)
+	{
+		quotient[i] /= normalizer;
+		int coef = quotient[i];
+
+		if (coef != 0) 
+		{
+			for (int j = 1; j < divisor.size(); j++)
+			{
+				quotient[i + j] += -divisor[j] * coef;
+			}
+        }
+
+	}
+    int s = quotient.size();
+    for (int i = s-1; i > s - divisor.size(); i--) {
+        quotient.pop_back();
+    }
+    std::reverse(quotient.begin(), quotient.end());
+    for (auto it : quotient) {
+        std::cout << it << " ";
+    }
+    std::cout << "\n";
+        return Polynomial<T>(quotient, zero_flag);
+
+}
+template<class T>
+Polynomial<T>Polynomial<T>::operator %(const Polynomial<T>& p) {
+    std::vector<T> dividend = this->coefficients;
+    std::reverse(dividend.begin(), dividend.end());
+    std::vector<T> divisor = p.coefficients;
+    std::reverse(divisor.begin(), divisor.end());
+    std::vector<T> quotient;
+	quotient = dividend;
+    
+	int normalizer = divisor[0];
+	
+	for (int i = 0; i < dividend.size() - (divisor.size() - 1); i++)
+	{
+		quotient[i] /= normalizer;
+		int coef = quotient[i];
+
+		if (coef != 0) 
+		{
+			for (int j = 1; j < divisor.size(); j++)
+			{
+				quotient[i + j] += -divisor[j] * coef;
+			}
+        }
+
+	}
+    std::vector<T> remainder;
+    int s = quotient.size();
+    for (int i = s-1; i > s - divisor.size(); i--) {
+        
+        remainder.push_back(quotient[i]);
+    }
+    
+        return Polynomial<T>(remainder, zero_flag);
+}
+template<class T>
+Polynomial<T>::Polynomial(int size, T zero_flag) : zero_flag(zero_flag) {
+    coefficients = std::vector<T>(size);
+}
+
+template<class T>
+void Polynomial<T>::populate_with_zeros(int p, int q) {
+    for (int i = p; i <= q; i++) {
+        coefficients[i] = zero_flag;
+    }
+}
+
+template<class T>
+Polynomial<T>::Polynomial(const Polynomial<T> &polynomial) {
+    this->zero_flag = polynomial.zero_flag;
+    for (int i = 0; i < polynomial.coefficients.size(); i++) {
+        this->coefficients.push_back(polynomial.coefficients[i]);
+    }
+}
+
+template<class T>
+void Polynomial<T>::clean() {
+    int i = coefficients.size() - 1;
+    while (coefficients[i] == zero_flag) {
+        coefficients.pop_back();
+        i--;
+    }
+}
+
+template<class T>
 std::ostream &operator<< (std::ostream &os, const Polynomial<T> &p) {
     int i = 1;
     os << p.coefficients[0] << " + ";
     for (; i < p.coefficients.size() - 1; i++) {
         os << p.coefficients[i] << "x^" << i << " + ";
     }
-    os << p.coefficients[i+1] << "x^" << i+1;
+    os << p.coefficients[i] << "x^" << i;
     return os;
 }
 
--- tst/Polynomial_tests.cpp
+++ tst/Polynomial_tests.cpp
@@ -10,8 +10,8 @@ using namespace std;
 TEST(Polynomial_test, basic_operands) {
     vector<int> v1 = {1,2,3,4};
     vector<int> v2 = {1,2,3,4,5,6};
-    auto p1 = Polynomial<int>(v1);
-    auto p2 = Polynomial<int>(v2);
+    auto p1 = Polynomial<int>(v1,0);
+    auto p2 = Polynomial<int>(v2,0);
     auto p3 = p1 + p2;
     for (auto item : p3.coefficients) {
         cout << item << endl;
@@ -22,10 +22,18 @@ TEST(Polynomial_test, basic_operands) {
 TEST(Polynomial_test, multiply) {
     vector<int> v1 = {4,3,1};
     vector<int> v2 = {-1,8,-2,4};
-    auto p1 = Polynomial<int>(v1);
-    auto p2 = Polynomial<int>(v2);
+    auto p1 = Polynomial<int>(v1,0);
+    auto p2 = Polynomial<int>(v2,0);
     auto p3 = p1*p2;
     vector<int> control = {-4,29,15,18,10,4};
     EXPECT_EQ(p3.coefficients, control);
     cout << p3;
+}
+TEST(Polynomial_test, vec) {
+    vector<int> v1 = {1,1,1,0,1,0,1,1};
+    vector<int> v2 = {1,1,0,1};
+    auto p1 = Polynomial<int>(v1,0);
+    auto p2 = Polynomial<int>(v2,0);
+    auto p3 = p1 % p2;
+    cout << p3 << endl;
 }
